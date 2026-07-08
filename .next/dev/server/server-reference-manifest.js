f239658be6f77892a8e6b939ee0aaa2acf169741
ebenezz <abenezerted3@gmail.com>
2026-07-08 08:51:26 +0300

    chore: cleared useless junks

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"kAP8Q9CPqiTB0jOUKQ0ShS1vjrziqIlMmYvsIc7bkXU=\"\n}"
--- .next/dev/static/chunks/app_layout_tsx_007e4b2._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__1ductli._.css","static/chunks/node_modules_next_dist_1nyev3z._.js","static/chunks/_1konwkz._.js"],
-    source: "dynamic"
-});
